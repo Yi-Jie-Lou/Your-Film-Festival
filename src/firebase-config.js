595a8ec2fecd4e1e82e155913f225e8a589416e5
Yi-Jie <dumpling426@gamil.com>
2022-04-11 12:09:58 +0800

    deploy to firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXxoQUDtYZTpNVNCmeZCQ97Co5rFrN6ic",
+  authDomain: "your-film-festival-d2cd4.firebaseapp.com",
+  projectId: "your-film-festival-d2cd4",
+  storageBucket: "your-film-festival-d2cd4.appspot.com",
+  messagingSenderId: "949992507014",
+  appId: "1:949992507014:web:7c9e03ee195d4929613307"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
