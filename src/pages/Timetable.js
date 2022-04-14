75c0dc89cd554ab8d0dcdd333b57207d89c5ee0b
Yi-Jie <dumpling426@gamil.com>
2022-04-14 10:45:38 +0800

    init tailwindcss

--- src/pages/Timetable.js
+++ src/pages/Timetable.js
@@ -6,7 +6,11 @@ function Timetable() {
   
 
   return (
-    <div>Hello
+    <div className=" flex-1 my-0 mx-auto w-4/5 border-2 rounded-lg">
+    <div className="text-1xl w-32 h-32 m-4 border-2 rounded ">Hello
+    </div>
+    <div className="text-1xl w-32 h-32 m-4 border-2 rounded ">Hello
+    </div>
     </div>
   );
 }
