61b1adea0204cb4556cd51cc0a0a26b462ffacfb
Yi-Jie <dumpling426@gamil.com>
2022-04-25 13:44:15 +0800

    add protect age sign of list

--- src/pages/Timetable.js
+++ src/pages/Timetable.js
@@ -26,7 +26,7 @@ function TimetableContainer() {
   }, [features]);
 
   useEffect(() => {
-    if (!initQureyDate.id) {
+    if (!initQureyDate.id && period[0]) {
       setQureyDate(period[0].dates);
     }
   }, [period]);
@@ -64,14 +64,11 @@ function TimetableContainer() {
 
         {locations &&
           locations.map((item, index) => (
-            <div
-              key={index}
-              className=" my-4 mx-auto w-4/5  rounded-lg"
-            >
+            <div key={index} className=" my-4 mx-auto w-4/5  rounded-lg">
               <div className=" flex mx-auto w-4/5 p-1 rounded-lg text-xl">
                 {item}
               </div>
-              <div className=" flex mx-auto w-4/5 h-72  border-2 border-stone-700 rounded-lg">
+              <div className=" flex mx-auto w-4/5 h-72   border-t-2 border-stone-700 ">
                 {queryTimetable.map((film, index) =>
                   film.location === item ? (
                     <NavLink
@@ -80,11 +77,13 @@ function TimetableContainer() {
                     >
                       <div
                         key={index}
-                        className="relative text-1xl w-64 h-64 m-4 border-2 rounded "
+                        className="relative text-1xl w-64 h-64 mr-4 my-4 border-2 rounded "
                       >
                         <div className="absolute flex flex-col justify-center h-full  w-full  text-white  opacity-0 hover:opacity-100 backdrop-blur-sm  ">
-                        <p className="w-3/4 mx-auto mt-2 text-lg text-center text-shadow"> {film.name} {film.start}-{film.end}</p>
-  
+                          <p className="w-3/4 mx-auto mt-2 text-lg text-center text-shadow">
+                            {" "}
+                            {film.name} {film.start}-{film.end}
+                          </p>
                         </div>
                         <img
                           className="w-full h-full object-cover"
@@ -99,6 +98,32 @@ function TimetableContainer() {
               </div>
             </div>
           ))}
+        <div className="w-2/3 mt-10 mx-auto">
+          <div className="flex my-2">
+            <div className="w-6 h-6 mr-2 bg-lime-500 text-center text-shadow-light text-white rounded-md">
+              普
+            </div>
+            <p>一般觀眾皆可觀賞。</p>
+          </div>
+          <div className="flex my-2">
+            <div className="w-6 h-6 mr-2 bg-blue-500 text-center text-shadow-light text-white rounded-md">
+              護
+            </div>
+            <p>未滿六歲之兒童不得觀賞，六歲以上十二歲未滿之兒童須父母、師長或成年親友陪伴輔導觀賞。</p>
+          </div>
+          <div className="flex my-2">
+            <div className="w-6 h-6 mr-2 bg-orange-500 text-center text-shadow-light text-white rounded-md">
+              輔
+            </div>
+            <p>未滿十五歲之人不得觀賞。</p>
+          </div>
+          <div className="flex my-2">
+            <div className="w-6 h-6 mr-2 bg-red-500 text-center text-shadow-light text-white rounded-md  ">
+              限
+            </div>
+            <p>未滿十八歲之人不得觀賞。</p>
+          </div>
+        </div>
       </div>
     </>
   );
